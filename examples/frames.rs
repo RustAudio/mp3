0fbfb7990e3ec0d3105f3cbfbac1e242059fedd2
est31 <MTest31@outlook.com>
2018-08-14 06:41:12 +0200

    Add frames example tool to dump header info of mp3 file frames

---
+++ examples/frames.rs
@@ -0,0 +1,18 @@
+extern crate mp3;
+
+use mp3::frame::FrameReader;
+
+use std::fs::File;
+use std::io::Read;
+
+fn main() {
+    let path = std::env::args().nth(1).unwrap();
+    let mut f = File::open(path).unwrap();
+    let mut buf = Vec::new();
+    f.read_to_end(&mut buf).unwrap();
+    let slice = mp3::trim_data(&buf).unwrap();
+    let mut frame_reader = FrameReader::new(slice);
+    while let Some(v) = frame_reader.next_frame().unwrap() {
+        println!("{:?}", v.header());
+    }
+}
